b21b71f74edc888f42b578c1db78df4689d0e5da
gizemmm <119964833+gzmaydn08@users.noreply.github.com>
2022-12-11 00:20:27 +0300

    Add files via upload

---
+++ day09incrementdecrementternaryswitch/Switch01.java
@@ -0,0 +1,67 @@
+package day09incrementdecrementternaryswitch;
+
+public class Switch01 {
+
+    public static void main(String[] args) {
+
+        //example 1) gün isimlerini verince kaçıncı gün olduğunu yazdıran kodu yazınız
+
+        //1.yol(if-else)
+
+        String dayName = "tuesday";
+        if (dayName.equalsIgnoreCase("sunday")){
+            System.out.println(1);
+        } else if (dayName.equalsIgnoreCase("monday")) {
+            System.out.println(2);
+        } else if (dayName.equalsIgnoreCase("tuesday")) {
+            System.out.println(3);
+        } else if (dayName.equalsIgnoreCase("wednesday")) {
+            System.out.println(4);
+        } else if (dayName.equalsIgnoreCase("thursday")) {
+            System.out.println(5);
+        } else if (dayName.equalsIgnoreCase("friday")) {
+            System.out.println(6);
+        } else if (dayName.equalsIgnoreCase("saturday")) {
+            System.out.println(7);
+        }else {
+            System.out.println("lütfen geçerli gün ismi giriniz.");
+        }
+
+
+
+        //2.yol(switch)
+
+        switch(dayName){
+
+            case "sunday":
+                System.out.println(1);
+                break;
+            case "monday":
+                System.out.println(2);
+                break;
+            case "tuesday":
+                System.out.println(3);
+                break;
+            case "wednesday":
+                System.out.println(4);
+                break;
+            case "thursday":
+                System.out.println(5);
+                break;
+            case "friday":
+                System.out.println(6);
+                break;
+            case "saturday":
+                System.out.println(7);
+                break;
+            default:
+                System.out.println("lütfen geçerli gün ismi giriniz......");
+        }
+
+
+
+
+
+
+    }
+}
